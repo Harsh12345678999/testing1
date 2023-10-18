74c2608480d611b1deff5e246e38ec25c75bb18f
Harsh Gautam <122910800+Harsh12345678999@users.noreply.github.com>
2023-10-18 14:25:07 +0530

    testing

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -12,7 +12,7 @@ import {
 export const getProduct = () => async (dispatch) => {
   try {
     dispatch({ type: ALL_PRODUCT_REQUEST })
-    const { data } = await axios.get("/api/v1/products")
+    const { data } = await axios.get("https://testing1-api.vercel.app/api/v1/products")
     dispatch({
       type: ALL_PRODUCT_SUCCESS,
       payload: data
